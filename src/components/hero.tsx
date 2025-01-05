3b0f6f4b79a03251fc76339e5a1f161dccf4d4d8
TobiasRW <tobiasrw98@gmail.com>
2025-01-05 23:57:53 +0100

    responsive styling

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,15 +1,28 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ArrowDown } from "@phosphor-icons/react/dist/ssr";
+import { motion, useDragControls, useMotionValue, animate } from "motion/react";
 
 export default function Hero() {
+  const controls = useDragControls();
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const rotate = useMotionValue(-12);
+
+  const handleDragEnd = () => {
+    animate(x, 0, { type: "spring", stiffness: 300, damping: 20 });
+    animate(y, 0, { type: "spring", stiffness: 300, damping: 20 });
+    animate(rotate, -12, { type: "spring", stiffness: 300, damping: 20 });
+  };
+
   return (
     <div className="h-screen w-screen bg-[#1A1A1A]">
-      <div className="relative flex h-2/4 w-screen items-center justify-center rounded-b-[40px] bg-background">
+      <div className="relative flex h-[420px] w-screen items-center justify-center rounded-b-[40px] bg-background sm:h-[450px] lg:h-[550px] xl:h-[600px]">
         <div className="absolute z-0 h-full w-full bg-[radial-gradient(rgba(0,0,0,0.1)_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_70%_70%_at_50%_50%,#000_70%,transparent_100%)] dark:bg-[radial-gradient(rgba(255,255,255,0.1)_1px,transparent_1px)]"></div>
-        <div className="z-10 mx-auto flex w-10/12 flex-col gap-6 pt-10">
-          <div className="relative mx-auto w-80">
-            <div className="mx-auto h-36 w-36 overflow-hidden rounded-full border-2 border-whitebg bg-gradient-to-tr from-[#d9d9d9] to-[#d1cccc] dark:border-none dark:from-[#1A1A1A] dark:to-[#2D2D2D]">
+        <div className="z-10 mx-auto flex w-10/12 flex-col gap-6 pt-10 lg:gap-10 lg:pt-16">
+          <div className="relative mx-auto flex w-72 items-center justify-center sm:w-80">
+            <div className="mx-auto h-32 w-32 overflow-hidden rounded-full border-2 border-whitebg bg-gradient-to-tr from-[#d9d9d9] to-[#d1cccc] sm:h-36 sm:w-36 lg:h-40 lg:w-40 xl:h-44 xl:w-44 dark:border-none dark:from-[#1A1A1A] dark:to-[#2D2D2D]">
               <Image
                 src="/images/portrait.webp"
                 alt="Hero"
@@ -18,14 +31,22 @@ export default function Hero() {
                 className=""
               />
             </div>
-            <div className="absolute -right-1 top-8 -rotate-12 rounded-full bg-whitebg px-3 py-2 font-heading text-base font-medium shadow-lg dark:bg-[#1d1d1d]">
+            <motion.div
+              className="absolute right-3 top-8 -rotate-12 cursor-pointer rounded-full bg-whitebg px-3 py-2 font-heading text-xs font-medium shadow-lg sm:right-1 sm:text-sm lg:-right-3 lg:text-base xl:top-10 dark:bg-[#1d1d1d]"
+              drag
+              dragControls={controls}
+              style={{ x, y, rotate }}
+              onDragEnd={handleDragEnd}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
               Tobias Wolmar 👋🏻
-            </div>
+            </motion.div>
           </div>
-          <h1 className="bg-gradient-to-br from-foreground from-50% to-background bg-clip-text text-center text-2xl font-semibold text-transparent">
+          <h1 className="bg-gradient-to-br from-foreground from-50% to-background bg-clip-text text-center text-2xl font-semibold text-transparent sm:text-3xl md:mx-auto md:w-9/12 lg:text-4xl xl:w-4/6 xl:text-5xl">
             Passioneret webudvikler med speciale i frontend-udvikling
           </h1>
-          <div className="flex justify-center gap-4">
+          <div className="flex justify-center gap-4 lg:gap-6 xl:gap-8">
             <Button variant="default">
               Projekter
               <ArrowDown size={32} weight="bold" />
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -3,109 +3,132 @@ import { motion } from "motion/react";
 import { useState } from "react";
 import { ArrowUpRight, ArrowDown } from "@phosphor-icons/react";
 
-export default function Nav() {
-  const [isOpen, setIsOpen] = useState(false);
+// Animation Variants
+const sideBarVariants = {
+  open: {
+    x: 0,
+    transition: { duration: 0.3, ease: "easeInOut" },
+  },
+  closed: {
+    x: "100%",
+    transition: { duration: 0.3, ease: "easeInOut" },
+  },
+};
 
-  const toggleMenu = () => {
+const hamburgerVariants = {
+  line1: { open: { rotate: 45, y: 7 }, closed: { rotate: 0, y: 0 } },
+  line2: { open: { opacity: 0 }, closed: { opacity: 1 } },
+  line3: {
+    open: { rotate: -45, y: -7, width: 24 },
+    closed: { rotate: 0, y: 0, width: 20 },
+  },
+};
+
+// stored links
+const links = {
+  github: "https://github.com/TobiasRW",
+  linkedin: "https://www.linkedin.com/in/tobias-wolmar-87991224a/",
+};
+
+// Navigation component
+export default function Nav() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu: React.MouseEventHandler<HTMLDivElement> = () => {
     setIsOpen(!isOpen);
   };
 
-  const sideBarVariants = {
-    open: {
-      x: 0,
-      transition: { duration: 0.3, ease: "easeInOut" },
-    },
-    closed: {
-      x: "100%",
-      transition: { duration: 0.3, ease: "easeInOut" },
-    },
-  };
-
   return (
-    <div className="">
-      <header className="fixed z-50 flex h-20 w-screen items-center justify-between px-4">
-        <p className="text-sm">tobiasrw98@gmail.com</p>
+    <>
+      <header className="fixed z-50 flex h-20 w-screen items-center justify-between px-4 lg:px-10 xl:px-16">
+        <p className="md:text-s text-xs lg:text-base">tobiasrw98@gmail.com</p>
         <nav className="">
+          {/* Hamburger Menu */}
           <motion.div
-            className="flex flex-col items-end gap-1"
+            className="flex flex-col items-end gap-1 lg:hidden"
             onClick={toggleMenu}
           >
             <motion.span
-              className="z-50 block h-1 w-7 rounded-full bg-foreground"
-              initial={false}
+              className="z-50 h-[3px] w-6 rounded-full bg-foreground"
+              variants={hamburgerVariants.line1}
               animate={isOpen ? "open" : "closed"}
-              variants={{
-                open: { rotate: 45, y: 8 },
-                closed: {
-                  rotate: 0,
-                  y: 0,
-                },
-              }}
-            ></motion.span>
+            />
             <motion.span
-              className="z-50 block h-1 w-9 rounded-full bg-foreground"
-              initial={false}
+              className="z-50 h-[3px] w-8 rounded-full bg-foreground"
+              variants={hamburgerVariants.line2}
               animate={isOpen ? "open" : "closed"}
-              variants={{
-                open: { opacity: 0 },
-                closed: {
-                  opacity: 1,
-                },
-              }}
-            ></motion.span>
+            />
             <motion.span
-              className="z-50 block h-1 w-6 rounded-full bg-foreground"
-              initial={false}
+              className="z-50 h-[3px] w-5 rounded-full bg-foreground"
+              variants={hamburgerVariants.line3}
               animate={isOpen ? "open" : "closed"}
-              variants={{
-                open: { rotate: -45, y: -8, width: 28 },
-                closed: {
-                  rotate: 0,
-                  y: 0,
-                  width: 24,
-                },
-              }}
-            ></motion.span>
+            />
           </motion.div>
-          <div className="">
-            <motion.div
-              className="absolute right-0 top-0 z-20 h-[100vh] w-[70%] md:w-[45%]"
-              variants={sideBarVariants}
-              animate={isOpen ? "open" : "closed"}
-              initial="closed"
+
+          {/* Menu items for laptops */}
+          <div className="hidden font-heading text-xl lg:flex lg:items-center lg:gap-10">
+            <a
+              href={links.github}
+              target="_blank"
+              className="group flex items-center gap-2"
             >
-              <div className="absolute top-0 z-[-2] h-screen w-screen bg-whitebg bg-[radial-gradient(100%_50%_at_50%_0%,rgba(36,64,200,0.15)_20%,rgba(0,163,255,0)_80%,rgba(0,163,255,0)_100%)] dark:bg-background dark:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(36,64,155,0.15)_20%,rgba(0,163,255,0)_80%,rgba(0,163,255,0)_100%)]"></div>
-              <ul className="absolute right-0 top-24 z-[100] flex flex-col gap-8 font-heading text-3xl font-medium">
-                <a
-                  href="https://github.com/TobiasRW"
-                  target="_blank"
-                  className="flex items-center gap-2 pr-32"
-                >
-                  <p>Github</p>
-                  <ArrowUpRight size={28} />
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/tobias-wolmar-87991224a/"
-                  target="_blank"
-                  className="flex items-center gap-2"
-                >
-                  <p>LinkedIn</p>
-                  <ArrowUpRight size={28} />
-                </a>
-                <hr className="h-1 w-10/12 rounded-full border-none bg-gradient-to-r from-[#1a1a1a] from-30% to-[#DADEE9] dark:from-[#DADEE9] dark:to-[#1a1a1a]" />
-                <li className="flex items-center gap-2">
-                  <p>Projekter</p>
-                  <ArrowDown size={28} />
-                </li>
-                <li className="flex items-center gap-2">
-                  <p>Om mig</p>
-                  <ArrowDown size={28} />
-                </li>
-              </ul>
-            </motion.div>
+              <p>Github</p>
+              <ArrowUpRight
+                size={16}
+                className="mt-1 transition-transform duration-200 group-hover:-translate-y-1 group-hover:scale-110"
+              />
+            </a>
+            <a
+              href={links.linkedin}
+              target="_blank"
+              className="group flex items-center gap-2"
+            >
+              <p>LinkedIn</p>
+              <ArrowUpRight
+                size={16}
+                className="mt-1 transition-transform duration-200 group-hover:-translate-y-1 group-hover:scale-110"
+              />
+            </a>
           </div>
+
+          {/* Sidebar for mobile & tablet */}
+          <motion.div
+            className="absolute right-0 top-0 z-20 h-[100vh] w-[70%] md:w-[45%] lg:hidden"
+            variants={sideBarVariants}
+            animate={isOpen ? "open" : "closed"}
+            initial="closed"
+          >
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-whitebg bg-[radial-gradient(100%_50%_at_50%_0%,rgba(36,64,200,0.15)_20%,rgba(0,163,255,0)_80%,rgba(0,163,255,0)_100%)] dark:bg-background dark:bg-[radial-gradient(100%_50%_at_50%_0%,rgba(36,64,155,0.15)_20%,rgba(0,163,255,0)_80%,rgba(0,163,255,0)_100%)]"></div>
+            <ul className="absolute left-10 top-24 z-[100] flex w-3/4 flex-col gap-8 font-heading text-2xl font-medium sm:left-20 sm:gap-12 sm:text-3xl">
+              <a
+                href={links.github}
+                target="_blank"
+                className="flex items-center gap-2"
+              >
+                <p>Github</p>
+                <ArrowUpRight size={28} />
+              </a>
+              <a
+                href={links.linkedin}
+                target="_blank"
+                className="flex items-center gap-2"
+              >
+                <p>LinkedIn</p>
+                <ArrowUpRight size={28} />
+              </a>
+              <hr className="h-1 w-10/12 rounded-full border-none bg-gradient-to-r from-[#1a1a1a] from-30% to-[#DADEE9] lg:hidden dark:from-[#DADEE9] dark:to-[#1a1a1a]" />
+              <li className="flex items-center gap-2">
+                <p>Projekter</p>
+                <ArrowDown size={28} />
+              </li>
+              <li className="flex items-center gap-2">
+                <p>Om mig</p>
+                <ArrowDown size={28} />
+              </li>
+            </ul>
+          </motion.div>
         </nav>
       </header>
-    </div>
+    </>
   );
 }
