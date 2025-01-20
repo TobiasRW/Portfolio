713f7df50436d865c0fdce9e976e029b3b93b6ff
TobiasRW <tobiasrw98@gmail.com>
2025-01-20 23:14:25 +0100

    text written for coelm project page

--- src/app/coelm/page.tsx
+++ src/app/coelm/page.tsx
@@ -10,42 +10,41 @@ export default function Page() {
     {
       title: "React",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "React bruges til at bygge interaktive brugergrænseflader med komponenter. Jeg har brugt det til at opbygge hele strukturen og funktionaliteten af websitet, hvilket har gjort det nemt at genbruge kode og opnå en effektiv udviklingsproces.",
       icon: "/icons/react.svg",
       website: "https://react.dev/",
     },
     {
       title: "Vite.js",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "Vite er et værktøj, der gør udviklingen hurtig og effektiv ved at levere øjeblikkelige opdateringer under kodning. Jeg har brugt det til at gøre udviklingsprocessen mere smidig og effektiv.",
       icon: "/icons/vitejs.svg",
       website: "https://vitejs.dev/",
     },
     {
       title: "Tailwind CSS",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "Tailwind er et CSS framework, der gør det nemt at opbygge en responsiv og moderne brugergrænseflade ved brug af utility classes. Jeg har brugt det til at style websitet og opnå en ensartet og moderne design.",
       icon: "/icons/tailwind.svg",
       website: "https://tailwindcss.com/",
     },
     {
       title: "Motion",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "Motion gør det supernemt at lave fede animationer i React. Jeg har brugt det til at skabe dynamiske elementer og animationer, der gør websitet mere levende og engagerende. Herunder bl.a. knapper og andre interaktive elementer.",
       icon: "/icons/framer-motion.svg",
       website: "https://motion.dev/",
     },
     {
       title: "JavaScript",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "JavaScript er brugt gennem React til at tilføje interaktivitet og dynamik til websitet. Jeg har anvendt det til at implementere funktioner og interaktioner, der gør brugeroplevelsen mere engagerende og intuitiv.",
       icon: "/icons/javascript.svg",
-      website: "",
     },
     {
       title: "GSAP",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. ",
+        "GSAP er et animationsbibliotek, der gør det nemt at lave avancerede og imponerende animationer. Jeg har brugt det til de mere komplekse animationer på websitet, som ses på 'om os'-siden og i webshoppen.",
       icon: "/icons/gsap.svg",
       website: "https://gsap.com/",
     },
@@ -66,9 +65,10 @@ export default function Page() {
         </div>
       </div>
       <ProjectDescription
-        text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. \n 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo. \n
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, nibh sit amet dapibus consequat, nibh est cursus sapien, tincidunt bibendum lectus neque eu leo.`}
+        text={`Dette projekt fokuserer på udviklingen af en ny visuel identitet samt et proof-of-concept webshop-design for Nimo Skin’s kommende brand, cølm. \n 
+        Projektet er udarbejdet i React med Vite som værktøj, hvilket har givet et hurtigt og effektivt udviklingsmiljø. Målet har været at skabe en engagerende og interaktiv side, hvilket er opnået ved hjælp af animationsbibliotekerne 'Motion' og 'GSAP'. \n
+        Til styling af siden har jeg anvendt Tailwind CSS, hvilket har gjort det enkelt at skabe en responsiv og moderne brugeroplevelse. Alt indhold på siden, herunder billeder skabt i Blender, samt design og animationer, er udviklet fra bunden. \n
+       Nimo Skin ønskede at udvikle en visuel identitet, der skiller sig ud fra deres eksisterende brand ved at fremstå mere minimalistisk, eksklusiv og moderne. Dette blev opnået gennem research af målgruppen, markedet samt Nimo Skin's værdier og visioner. Resultatet er et design og en webshop, der opnår netop dette. `}
         icons={[
           "/icons/react.svg",
           "/icons/vitejs.svg",
@@ -77,6 +77,7 @@ export default function Page() {
           "/icons/javascript.svg",
           "/icons/gsap.svg",
         ]}
+        highlightWords={["React", "Vite", "GSAP", "Motion", "Tailwind", "CSS"]}
       />
       <div className="mx-auto xl:grid xl:w-10/12 xl:grid-cols-[1.5fr,1fr] xl:gap-14 xl:py-40">
         <Slider
