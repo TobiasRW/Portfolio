d5488268d56e7f7da801b72366407cd1c86bce3a
TobiasRW <tobiasrw98@gmail.com>
2025-02-07 18:03:57 +0100

    next-international setup

---
+++ src/locales/da.ts
@@ -0,0 +1,40 @@
+// locales/da.ts
+export default {
+  navigation: {
+    back: "Tilbage",
+    projects: "Projekter",
+    about: "Om",
+  },
+  hero: {
+    title: "Passioneret webudvikler med speciale i frontend-udvikling",
+    button: {
+      projects: "Projekter",
+      about: "Om Mig",
+    },
+  },
+  projects: {
+    title: "Projekter",
+    project: {
+      coelm: {
+        name: "Coelm",
+        brief: "Hovedopgave som multimediedesigner",
+      },
+      bookBuddy: {
+        name: "Book Buddy",
+        brief: "En bog app til læsere til at organisere deres bøger",
+      },
+      lendr: {
+        name: "Lendr",
+        brief: "En app til udlejning af kufferter og rejsetasker",
+      },
+      glassFill: {
+        name: "Glass Fill",
+        brief: "Interaktivt website med scroll-effekter",
+      },
+      liveScores: {
+        name: "Live Scores",
+        brief: "En fodbold livescore app",
+      },
+    },
+  },
+} as const;
---
+++ src/locales/en.ts
@@ -0,0 +1,40 @@
+// locales/en.ts
+export default {
+  navigation: {
+    back: "Go Back",
+    projects: "Projects",
+    about: "About",
+  },
+  hero: {
+    title: "Passionate web developer specializing in frontend development",
+    button: {
+      projects: "Projects",
+      about: "About",
+    },
+  },
+  projects: {
+    title: "Projects",
+    project: {
+      coelm: {
+        name: "Coelm",
+        brief: "Final project as a multimedia design student",
+      },
+      bookBuddy: {
+        name: "Book Buddy",
+        brief: "A book app for readers to organize their books",
+      },
+      lendr: {
+        name: "Lendr",
+        brief: "An app for renting suitcases and travel bags",
+      },
+      glassFill: {
+        name: "Glass Fill",
+        brief: "Interactive website with scroll effects",
+      },
+      liveScores: {
+        name: "Live Scores",
+        brief: "A football live score app",
+      },
+    },
+  },
+} as const;
