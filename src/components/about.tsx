cf7e8b3f7f8e52d30504f09a7440955c8c27bd31
TobiasRW <tobiasrw98@gmail.com>
2025-06-26 17:14:25 +0200

    updated styling for project-cards

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,7 +3,7 @@ export default async function About() {
   const scopedT = await getScopedI18n("frontPage.about");
   return (
     <section id="about">
-      <div className="mx-auto flex w-10/12 flex-col gap-8">
+      <div className="mx-auto flex w-10/12 flex-col gap-8 lg:w-11/12 xl:w-10/12">
         <h2 className="text-center font-heading text-2xl font-semibold sm:text-3xl md:text-4xl xl:text-5xl">
           {scopedT("title")}
         </h2>
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -29,7 +29,7 @@ export default function ProjectCard({
 
   return (
     <div
-      className={`group relative mx-auto h-[365px] w-10/12 overflow-hidden rounded-lg shadow-lg xs:h-96 xs:w-[350px] sm:h-96 sm:w-[350px] md:h-80 md:w-72 lg:h-96 lg:w-[350px] 2xl:h-[440px] 2xl:w-[90%] 3xl:h-[550px] ${
+      className={`group relative mx-auto h-[365px] w-10/12 overflow-hidden rounded-lg shadow-lg xs:h-96 xs:w-[350px] sm:h-96 sm:w-[350px] md:h-72 md:w-full lg:h-[350px] lg:w-11/12 xl:h-[420px] xl:w-[90%] xl:max-w-[450px] 2xl:h-[480px] ${
         isPhone || isComputer ? "" : "bg-white"
       }`}
       style={
@@ -38,14 +38,14 @@ export default function ProjectCard({
     >
       <TransitionLink href={link}>
         <div
-          className={`absolute h-full w-full cursor-pointer transition-all duration-300 ${
+          className={`absolute h-full w-[110%] cursor-pointer transition-all duration-300 ${
             isPhone // Phone img style
-              ? "bottom-[-35%] left-[18%] rotate-[-10deg] scale-[1.3] group-hover:scale-[1.4]"
+              ? "bottom-[-35%] left-[18%] rotate-[-10deg] scale-[1.3] group-hover:scale-[1.4] md:scale-[1.2] md:group-hover:scale-[1.3] xl:scale-[1.3] xl:group-hover:scale-[1.4]"
               : isComputer // Computer img style
-                ? "bottom-[-32%] left-[-15%] scale-[1.4] group-hover:scale-150"
+                ? "bottom-[-32%] left-[-15%] scale-[1.4] group-hover:scale-150 md:bottom-[-35%] md:left-[-22%] md:scale-[1.2] md:group-hover:scale-[1.3] xl:bottom-[-32%] xl:scale-[1.4] xl:group-hover:scale-150"
                 : isPhone2 // Phone2 img style
-                  ? "bottom-[-18%] left-[20%] scale-[1.5] group-hover:scale-[1.6]"
-                  : "group-hover:scale-110" // Default img style
+                  ? "bottom-[-18%] left-[20%] scale-[1.5] group-hover:scale-[1.6] md:scale-[1.3] md:group-hover:scale-[1.4] xl:scale-[1.5] xl:group-hover:scale-[1.6]"
+                  : "left-[-5%] group-hover:scale-110" // Default img style
           }`}
         >
           <Image
@@ -58,7 +58,7 @@ export default function ProjectCard({
         </div>
         <div className="absolute top-4 mx-auto flex w-full flex-col gap-1 px-4">
           <div className="flex items-center justify-center">
-            <h3 className="font-heading text-3xl font-medium text-white lg:text-4xl 3xl:text-5xl">
+            <h3 className="font-heading text-3xl font-medium text-white md:text-2xl lg:text-4xl 3xl:text-5xl">
               {title}
             </h3>
             <div className="ml-auto rounded-full bg-white p-2 transition-all duration-300 group-hover:scale-90 lg:p-3 3xl:p-4">
