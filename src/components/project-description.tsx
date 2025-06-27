94e5bcd20415634809c131bcd7f21917e1423388
TobiasRW <tobiasrw98@gmail.com>
2025-06-28 00:22:10 +0200

    refactor of project techstack icons

--- src/components/project-description.tsx
+++ src/components/project-description.tsx
@@ -71,30 +71,35 @@ export default function ProjectDescription({
           <hr className="h-[1px] border-none bg-foreground lg:hidden" />
 
           {/* Icons Section */}
-          <div className="flex flex-wrap gap-4 rounded-2xl lg:h-full lg:w-full lg:gap-6 lg:bg-[#EDECEC] lg:p-4 xl:w-4/6 xl:p-6 3xl:w-2/6 lg:dark:bg-background">
-            {iconLabels.map((iconLabel, index) => {
-              const logoData = getLogoByLabel(iconLabel, isDarkMode);
-              return (
-                <div
-                  key={index}
-                  className="lg:dark:bg-[#1A1A1A flex items-center justify-center gap-2 rounded-full bg-[#EDECEC] p-4 lg:bg-whitebg dark:bg-[#2C2C2C]"
-                >
-                  <a
-                    href={logoData.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
+          <div className="flex flex-col gap-4 rounded-2xl lg:w-full lg:self-start xl:w-4/6 3xl:w-2/6">
+            <p className="hidden font-heading text-2xl font-semibold lg:block">
+              Techstack
+            </p>
+            <div className="flex flex-wrap gap-4">
+              {iconLabels.map((iconLabel, index) => {
+                const logoData = getLogoByLabel(iconLabel, isDarkMode);
+                return (
+                  <div
+                    key={index}
+                    className="flex h-12 w-12 items-center justify-center gap-2 rounded-full bg-[#EDECEC] p-4 lg:h-14 lg:w-14 dark:bg-[#2C2C2C]"
                   >
-                    <Image
-                      src={logoData.icon}
-                      alt={`icon-${index}`}
-                      width={24}
-                      height={24}
-                      className="h-6 w-6 transition-all duration-300 hover:scale-90 xl:h-7 xl:w-7 2xl:h-8 2xl:w-8"
-                    />
-                  </a>
-                </div>
-              );
-            })}
+                    <a
+                      href={logoData.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src={logoData.icon}
+                        alt={`icon-${index}`}
+                        width={24}
+                        height={24}
+                        className="h-6 w-6 transition-all duration-300 hover:scale-90 xl:h-7 xl:w-7 2xl:h-8 2xl:w-8"
+                      />
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
