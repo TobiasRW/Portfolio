8896434e439fa42e737d4f0c33763e486232aeaf
TobiasRW <tobiasrw98@gmail.com>
2025-09-13 18:06:01 +0200

    update highlight text functiom

--- src/helpers/highlightText.tsx
+++ src/helpers/highlightText.tsx
@@ -1,13 +1,13 @@
 const highlightText = (text: string, highlightWords: string[]) => {
-  const lowerHighlightWords = highlightWords.map((hw) => hw.toLowerCase()); // Normaliser
+  const lowerHighlightWords = highlightWords.map((hw) => hw.toLowerCase());
   return text.split(" ").map((word, index) => {
-    const cleanWord = word.replace(/[,!?]/g, "").toLowerCase(); // Fjern tegnsætning og normaliser
+    const cleanWord = word.replace(/[,!?.]/g, "").toLowerCase();
     const isHighlighted = lowerHighlightWords.includes(cleanWord);
 
     return (
       <span key={index} className={isHighlighted ? "font-normal italic" : ""}>
         {word}
-        {index < text.split(" ").length - 1 && " "} {/* Tilføj mellemrum */}
+        {index < text.split(" ").length - 1 && " "}
       </span>
     );
   });
