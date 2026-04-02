4c782f1081fd93b07097671c0ed16132057d5683
TobiasRW <tobiasrw98@gmail.com>
2026-04-02 20:01:32 +0200

    custom media postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 export default {
   plugins: {
+    '@csstools/postcss-global-data': {
+      files: ['./src/app/[locale]/media.css'],
+    },
+    'postcss-custom-media': {},
     'postcss-nested': {},
   },
 };
